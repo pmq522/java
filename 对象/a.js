cc80261242cc7ed9d3519575ecc18c6062803e4c
pmq522 <43670138+pmq522@users.noreply.github.com>
2019-04-12 09:46:48 +0800

    object1

---
+++ 对象/a.js
@@ -0,0 +1,5 @@
+function init(){
+  var idTag = document.getElementById('xiaozhu')
+  idTag.setAttribute('style','height: 150px;width: 180px;background-color: pink;margin-top:318px;margin-left:522px')
+}
+window.onload = init
